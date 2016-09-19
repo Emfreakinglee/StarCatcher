31a06d0ffbefadb10e70a204db87d3b4b4c347a1
Emily <limeyleelove@gmail.com>
2016-09-19 13:53:34 -0600

    test

---
+++ StarCatcher/Assets/Scripts/Level/RecylceLevelObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecylceLevelObject : MonoBehaviour {
+
+    private Vector3 newLocation;
+
+	void OnTriggerEnter()
+    {
+        newLocation.x = StaticVars.nextSectionPosition;
+        transform.position = newLocation;
+        StaticVars.nextSectionPosition += StaticVars.distance;
+    }
+}
